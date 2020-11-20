7cac200b62ce2096f61737340e76ed91b3872736
José Ferreira <73367242+jpenetroferreira@users.noreply.github.com>
2020-11-20 16:05:12 +0000

    Add files via upload

--- detection_v1.py
+++ detection_v1.py
@@ -2,16 +2,23 @@ import cv2
 import numpy as np
 import argparse
 
-image = cv2.imread("cap_light.jpg")
+image = cv2.imread("Images\\no_flash.jpeg")
 
 ## just to resize image that's too big
-image = cv2.resize(image,None,fx=0.5,fy=0.5, interpolation = cv2.INTER_CUBIC)
+image = cv2.resize(image,None,fx=0.2,fy=0.2, interpolation = cv2.INTER_CUBIC)
 
 # defining color boundaries - list of tuples with RGB values of upper and lower limits of the color
 # we must define all the colors present
 # red  white dark_green  light_green  yellow  orange
 boundaries = [
-    ([17,15,100],[50,56,200], 'red') 
+    ([40,20,200],[55,35,240], 'red'), 
+    ([80,100,0],[100,120,35], 'dark green'),
+    ([185,200,160],[210,220,180], 'light green'),
+    ([200,220,220],[255,255,255], 'white'),
+    ([70,180,200],[90,195,225], 'nude'),
+    ([0,200,220],[5,225,245], 'yellow'),
+    ([70,150,220],[85,170,245], 'orange')
+    
 ]
 
 for (lower,upper, color) in boundaries:
