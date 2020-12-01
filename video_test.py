e5d78da6f1eafb1465b10a239459798d1dae2dd5
José Ferreira <73367242+jpenetroferreira@users.noreply.github.com>
2020-12-01 20:56:25 +0000

    Add files via upload

--- video_test.py
+++ video_test.py
@@ -1,12 +1,47 @@
-import numpy as cv 
+import numpy as np
 import cv2 as cv
 
 
 
 
+def area(w, h):
+    return w * h
+
+
+def color_in_frame(image):
+
+    boundaries = [
+    ([40,20,200],[55,35,240], 'red'), 
+    ([80,100,0],[100,120,35], 'dark green'),
+    ([185,200,160],[210,220,180], 'light green'),
+    ([200,220,220],[255,255,255], 'white'),
+    ([70,180,200],[90,195,225], 'nude'),
+    ([0,200,220],[5,225,245], 'yellow'),
+    ([70,150,220],[85,170,245], 'orange')
+    ]
+
+
+    for (lower,upper, color) in boundaries:
+        lower = np.array(lower, dtype = 'uint8')
+        upper = np.array(upper, dtype= 'uint8')
+        color = color
+
+    if cv.inRange(image, lower, upper).any():
+        mask = cv.inRange(image, lower, upper)
+        print("Color detected: " + color)
+        output = cv.bitwise_and(image, image, mask=mask)
+    else:
+        print("Color not detected")
+
+    if 'output' in vars():
+        cv.imshow("image",np.hstack([image,output]))
+        
+
+
 def thresh_callback(image, gray_image, val):
     
-
+    
+    
     threshold = val
 
     canny_output = cv.Canny(gray_image, threshold, threshold * 2)
@@ -17,6 +52,8 @@ def thresh_callback(image, gray_image, val):
 
     contours_poly = [None] * len(contours)
     boundRect = [None] * len(contours)
+    
+    
 
 
     ## boundingRect returns x, y, w, h -> w is width and h is height
@@ -26,13 +63,9 @@ def thresh_callback(image, gray_image, val):
     for i, c in enumerate(contours):
         contours_poly[i] = cv.approxPolyDP(c,3,True)
         boundRect[i] = cv.boundingRect(contours_poly[i])
-        
     
-    print(boundRect)
-
     
 
-    
     final_img = image
 
     for i in range(len(contours)):
@@ -61,6 +94,10 @@ cap = cv.VideoCapture("video.mp4")
 while(cap.isOpened()):
 
     ret, frame = cap.read()
+    
+
+    if not ret:
+        break
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     gray = cv.blur(gray, (3,3))
@@ -71,11 +108,16 @@ while(cap.isOpened()):
     thresh = 100
 
     thresh_callback(frame, gray, thresh)
+    color_in_frame(frame)
 
 
 
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(0) == ord('q'):
         break
+
+
+
 
 cap.release()
 cv.destroyAllWindows()
+
